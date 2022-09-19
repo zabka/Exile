9f502729d73d232bcc6834e7f11054f4311f98a7
Izzy Blacklock <izzy.blacklock@gmail.com>
2022-09-18 22:24:54 -0600

    Removed auto protection from most items
    Removed it from crafting stations. Nails will provide new way to
    selectively protect nodes.  Glass remains protected as does
    antiq nodes. Anything already protected remains protected.

--- mods/tech/craft_stations.lua
+++ mods/tech/craft_stations.lua
@@ -336,15 +336,6 @@ minetest.register_node("tech:chopping_block", {
 	groups        = {dig_immediate = 3, falling_node = 1, temp_pass = 1},
 	sounds        = nodes_nature.node_sound_wood_defaults(),
 	on_rightclick = crafting.make_on_rightclick("chopping_block", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 ------------------------------
@@ -372,15 +363,6 @@ minetest.register_node("tech:mortar_pestle_basalt",{
 		},
 	sounds        = nodes_nature.node_sound_stone_defaults(),
 	on_rightclick = crafting.make_on_rightclick("mortar_and_pestle", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 minetest.register_node("tech:mortar_pestle_granite",{
@@ -405,15 +387,6 @@ minetest.register_node("tech:mortar_pestle_granite",{
 		},
 	sounds        = nodes_nature.node_sound_stone_defaults(),
 	on_rightclick = crafting.make_on_rightclick("mortar_and_pestle", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 minetest.register_node("tech:mortar_pestle_limestone",{
@@ -438,15 +411,6 @@ minetest.register_node("tech:mortar_pestle_limestone",{
 		},
 	sounds        = nodes_nature.node_sound_stone_defaults(),
 	on_rightclick = crafting.make_on_rightclick("mortar_and_pestle", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 -------------------
@@ -470,16 +434,7 @@ minetest.register_node("tech:anvil", { --anvil--metal  working
 	groups        = {dig_immediate=3, falling_node = 1, temp_pass = 1},
 	sounds        = nodes_nature.node_sound_stone_defaults(),
 	on_rightclick = crafting.make_on_rightclick("anvil", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
-})
+	})
 
 minetest.register_node("tech:carpentry_bench", { --carpentry_bench--more sophisticated wood working
 	description   = S("Carpentry Bench"),
@@ -503,15 +458,6 @@ minetest.register_node("tech:carpentry_bench", { --carpentry_bench--more sophist
 			 flammable = 8},
 	sounds        = nodes_nature.node_sound_wood_defaults(),
 	on_rightclick = crafting.make_on_rightclick("carpentry_bench", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 minetest.register_node("tech:masonry_bench", { --masonry_bench--more sophisticated stone crafts
@@ -538,15 +484,6 @@ minetest.register_node("tech:masonry_bench", { --masonry_bench--more sophisticat
 	groups        = {dig_immediate=3, falling_node = 1, temp_pass = 1},
 	sounds        = nodes_nature.node_sound_wood_defaults(),
 	on_rightclick = crafting.make_on_rightclick("masonry_bench", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 --brick_makers_bench
@@ -581,15 +518,6 @@ minetest.register_node("tech:brick_makers_bench", {
 			 flammable = 8},
 	sounds        = nodes_nature.node_sound_wood_defaults(),
 	on_rightclick = crafting.make_on_rightclick("brick_makers_bench", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 --spinning_wheel
@@ -618,15 +546,6 @@ minetest.register_node("tech:spinning_wheel", {
 			 flammable = 8},
 	sounds        = nodes_nature.node_sound_wood_defaults(),
 	on_rightclick = crafting.make_on_rightclick("spinning_wheel", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 --Loom--turn fibre into fabric items
@@ -660,15 +579,6 @@ minetest.register_node("tech:loom", {
 			 flammable = 8},
 	sounds        = nodes_nature.node_sound_wood_defaults(),
 	on_rightclick = crafting.make_on_rightclick("loom", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 --Glassworking Furnace
@@ -694,15 +604,6 @@ minetest.register_node("tech:glass_furnace", {
 	groups        = {dig_immediate=3, falling_node = 1, temp_pass = 1},
 	sounds        = nodes_nature.node_sound_wood_defaults(),
 	on_rightclick = crafting.make_on_rightclick("glass_furnace", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
 	})
 
 ---------------------------------------
@@ -931,15 +832,6 @@ if legacy_stations == true then
         groups        = {dig_immediate = 3, falling_node = 1, temp_pass = 1},
         sounds        = nodes_nature.node_sound_wood_defaults(),
         on_rightclick = crafting.make_on_rightclick("chopping_block", 2, { x = 8, y = 3 }),
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local imeta = itemstack:get_meta()
-		minimal.metadata.after_place_node(imeta,meta)
-	end,
-	preserve_metadata = function(pos, oldnode, oldmeta, drops)
-		local imeta=drops[1]:get_meta()
-		minimal.metadata.preserve_metadata(imeta,oldmeta)
-	end,
    })
    --hammering_block
    --crude hammering crushing jobs,
