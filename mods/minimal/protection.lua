aed5a9342a8b2615cbd252063449e4deb03df36a
Izzy Blacklock <izzy.blacklock@gmail.com>
2022-09-18 21:22:02 -0600

    Added protection nails Image provided by voxeel Initial tests look good.  Nailes appear in crafting and inventory Clicking on a node with nails adds owner to the node clicked
    Probably need a longer click delay for using; maybe a on_rightclick
    instead of on_use

--- mods/minimal/protection.lua
+++ mods/minimal/protection.lua
@@ -1,6 +1,23 @@
 -- minimal/protection.lua
 --
--- 
+-- This may need to moved someplace else eventually.
+
+S=minimal.S
+
+
+function minimal.protection_nail_use( itemstack, user, pointed_thing )
+	local pt_pos=minetest.get_pointed_thing_position(pointed_thing,false)
+	local pt_node=minetest.get_node(pt_pos)
+	local pt_meta=minetest.get_meta(pt_pos)
+	local owner = user:get_player_name()
+	if not pt_meta:contains('owner') then
+		pt_meta:set_string("owner", owner)
+		itemstack:take_item()
+		minimal.infotext_merge(pt_pos, nil, pt_meta)
+	end
+	return itemstack
+end
+
 
 -- Set owner for protected items.
 function minimal.protection_after_place_node( pos, placer, itemstack, pointed_thing )
@@ -12,3 +29,7 @@ function minimal.protection_after_place_node( pos, placer, itemstack, pointed_th
 	minimal.infotext_merge(pos,nil,meta)
 	return (creative and creative.is_enabled_for and creative.is_enabled_for(pn))
 end
+
+
+
+
