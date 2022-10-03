571214027749e87eb5c478b48178a32e721dbd1c
Jeremy <jeremylshannon@gmail.com>
2022-10-02 22:51:03 -0700

    Handle "-dev" string in patch number for development versions of Minetest

--- mods/minimal/compat.lua
+++ mods/minimal/compat.lua
@@ -6,12 +6,13 @@ local version = minetest.get_version()
 local tabstr = string.split(version.string,".")
 local major = tonumber(tabstr[1])
 local minor = tonumber(tabstr[2])
-local patch = tonumber(tabstr[3])
+local dev = tostring(string.match(tabstr[3], "-dev") ~= nil)
+-- '%d[%d]*' extracts the first string of consecutive numeric chars
+local patch = tonumber(string.match(tabstr[3], '%d[%d]*'))
 minetest.log("action", "Running on version: "..version.project.." "..
-	     major.."."..minor.."."..patch)
+	     major.."."..minor.."."..patch.." Dev version: "..dev)
 minimal.mtversion = { project = version.project, major = major,
-		      minor = minor, patch = patch }
-print("VERSION : ",dump(minimal.mtversion))
+		      minor = minor, patch = patch, dev = dev }
 
 function minimal.mt_required_version(maj, min, pat)
    if minimal.mtversion.project ~= "Minetest" then
